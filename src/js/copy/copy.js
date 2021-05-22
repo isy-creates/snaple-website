a067182e86ff2d58a077ec4f6cc6fd2cc741a4e8
Isy <hallowelt@isy-creates.de>
2021-05-22 13:58:23 +0200

    added feedback to copy function

--- src/js/copy/copy.js
+++ src/js/copy/copy.js
@@ -13,16 +13,37 @@ class Copy {
 
   copyCode(e) {
     e.preventDefault();
-    var target = e.target.closest(".uagb-tabs__body");
-    var code = target.querySelector(".wp-block-code code");
-    var range = document.createRange();
+    let target = e.target.closest(".uagb-tabs__body");
+    let code = target.querySelector(".wp-block-code code");
+    let range = document.createRange();
 
     range.selectNode(code);
     window.getSelection().removeAllRanges(); //clear current selection
     window.getSelection().addRange(range); //select the text
     document.execCommand("copy"); // copy to clipboard
     window.getSelection().removeAllRanges(); // deselect everything
+    this.successMessage(e);
+  }
+
+  successMessage(e) {
+    let message = document.createElement("p");
+    message.classList.add("messages");
+    let node = document.createTextNode("Code copied");
+    message.appendChild(node);
+
+    let button = e.target.parentNode;
+    button.appendChild(message);
+
+    let removeButton = setTimeout(() => {
+      button.removeChild(message);
+    }, 1000);
   }
 }
 
 export default Copy;
+
+//1. create constructor
+//2. create copyCode
+//3. create successMessage
+//4. create error message
+//5. create mobile version
