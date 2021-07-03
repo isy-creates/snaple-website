5444d9887ad373f0b402274fd0785e9b627c87e1
Isy <hallowelt@isy-creates.de>
2021-07-03 23:19:00 +0200

    finished reworking single.php & style cleanup

--- single.php
+++ single.php
@@ -8,19 +8,7 @@
       $cat_name = lcfirst($categories[0]->cat_name);
     ?>
 
-    <article class="pattern__headline">
-      <div class="tagline">
-        <?php 
-                  $posttags = get_the_tags(); 
-                  if ($posttags) {
-                    foreach($posttags as $tag) {
-                      echo "<span class=\"tag\">" . $tag->name . '</span>'; 
-                    }
-                  }
-                ?>
-      </div>
-      <h1 class="headline h1"><span class="focus"><?php the_title(); ?></span> Pattern</h1>
-    </article>
+
     <div class="pattern__preview">
       <div class="preview-<?php print strtolower(get_the_title()); ?> pattern__resizeable <?php echo $cat_name  ?>">
         <?php the_field('preview_code'); ?>
@@ -28,26 +16,21 @@
       </div>
     </div>
     <div class="pattern__info">
-      <div class="pattern__code">
-        <h2 class="headline h2">How to <strong>code</strong> the <?php the_title(); ?> pattern</h2>
-        <?php  echo get_the_content() ?>
-        <div class="codepen">
-          <a href="<?php the_field('codepen_link'); ?>" target="_blank" rel="noopener noreferrer"
-            class="codepen__link small">Play with it on
-            Codepen</a>
+      <article class="pattern__headline">
+        <div class="tagline">
+          <?php 
+                  $posttags = get_the_tags(); 
+                  if ($posttags) {
+                    foreach($posttags as $tag) {
+                      echo "<span class=\"tag\">" . $tag->name . '</span>'; 
+                    }
+                  }
+                ?>
         </div>
-      </div>
-      <div class="pattern__use">
-        <?php if( get_field('usecase') ): ?>
-        <h3 class="headline h3"><strong>Use</strong> the <?php the_title(); ?> pattern</h3>
-        <?php the_field('usecase', false); ?>
-        <?php endif; ?>
-      </div>
-      <div class="pattern__accessebility">
-        <?php if( get_field('accessebility') ): ?>
-        <h3 class="headline h3"><?php the_title(); ?> pattern <strong>accessibility</strong> tipps</h3>
-        <?php the_field('accessebility', false); ?>
-        <?php endif; ?>
+        <h1 class="headline h1"><?php the_title(); ?> Pattern</h1>
+      </article>
+      <div class="pattern__code">
+        <?php  echo get_the_content() ?>
       </div>
     </div>
     <?php endwhile; endif; ?>
